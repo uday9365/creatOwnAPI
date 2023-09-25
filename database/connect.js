9265d91f2c1235b1134fd4143671049e5ffd5b91
Udayjit <udayhandique111@gmail.com>
2023-09-25 22:16:22 +0530

    env file introduce

--- database/connect.js
+++ database/connect.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose")
 const { options } = require("../routes/products")
 
-uri = "mongodb+srv://uday:5so8H7qZFV7aCcjE@udayapi.vsfoz9c.mongodb.net/UdayAPI?retryWrites=true&w=majority"
-
-const connectDB = () => {
+const connectDB = (uri) => {
   // console.log("It's working nicely");
   return mongoose.connect(uri, { 
     useNewUrlParser: true,
